a5fc797d9109fcb6e8fd22880a79a972d68842e1
codymoose <rcmoose2000@gmail.com>
2016-11-01 20:15:54 -0500

    Added color and sector customizability to Digital Doily

--- web-applications/DigitalDoily/DigitalDoily.js
+++ web-applications/DigitalDoily/DigitalDoily.js
@@ -1,8 +1,20 @@
 var sectors = 30;
-var sectorA = 360 / sectors;
+var sectorAngle = 360 / sectors;
 var pMX;
 var pMY;
+var canvas;
 var resetBtn;
+var sectorSlider;
+var sectorDiv;
+var r = 255;
+var g = 255;
+var b = 255;
+var rSlider;
+var rDiv;
+var gSlider;
+var gDiv;
+var bSlider;
+var bDiv;
 
 function setup(){
 	angleMode(DEGREES);
@@ -10,12 +22,47 @@ function setup(){
 	translate(width / 2, height / 2);
 	reset();
 	resetBtn = createButton("Reset");
-	resetBtn.position(5, 605);
+	resetBtn.position(5, height + 5);
 	resetBtn.mousePressed(reset);
+	sectorSlider = createSlider(1,45,30,1);
+	sectorSlider.position(resetBtn.x, resetBtn.y + resetBtn.height + 5)
+	sectorDiv = createDiv("Sectors: " + sectors);
+	sectorDiv.position(sectorSlider.x + sectorSlider.width + 5, sectorSlider.y);
+
+	rSlider = createSlider(0,255,255,1);
+	rSlider.position(sectorSlider.x, sectorSlider.y + sectorSlider.height + 5)
+	rDiv = createDiv("Red: " + r);
+	rDiv.position(rSlider.x + rSlider.width + 5, rSlider.y);
+
+	gSlider = createSlider(0,255,255,1);
+	gSlider.position(rSlider.x, rSlider.y + rSlider.height + 5)
+	gDiv = createDiv("Green: " + g);
+	gDiv.position(gSlider.x + gSlider.width + 5, gSlider.y);
+
+	bSlider = createSlider(0,255,255,1);
+	bSlider.position(gSlider.x, gSlider.y + gSlider.height + 5)
+	bDiv = createDiv("Blue: " + b);
+	bDiv.position(bSlider.x + bSlider.width + 5, bSlider.y);
 }
 
 function draw() {
 	translate(width/2,height/2);
+	if(sectors != sectorSlider.value()){
+		sectors = sectorSlider.value();
+		sectorAngle = 360 / sectors;
+		sectorDiv.html("Sectors: " + sectors);
+		reset();
+	}
+	
+	if(r != rSlider.value() || g != gSlider.value() || b != bSlider.value()){
+		r = rSlider.value();
+		g = gSlider.value();
+		b = bSlider.value();
+		rDiv.html("Red: " + r);
+		gDiv.html("Green: " + g);
+		bDiv.html("Blue: " + b);
+	}
+	stroke(r,g,b);
 	pMX = mouseX - width / 2;
 	pMY = mouseY - height / 2;
 }
@@ -24,25 +71,25 @@ function mouseDragged() {
 	push();
 	for(var i = 0; i < sectors; i++){
 		line(pMX, pMY, mouseX - width / 2, mouseY - height / 2);
-		rotate(sectorA);
+		rotate(sectorAngle);
 	}
 	pop();
 	
 	push();
 	for(var i = 0; i < sectors; i++){
 		line(pMY, pMX, mouseY - height / 2, mouseX - width / 2);
-		rotate(sectorA);
+		rotate(sectorAngle);
 	}
 	pop();
 }
 
 function reset(){
 	background(51);
-	stroke(255);	
+	stroke(100);	
 	push();
 	for(var i = 0; i < sectors; i++){
 		line(0, 0, width, height);
-		rotate(sectorA);
+		rotate(sectorAngle);
 	}
 	pop();
 }
