10499d6c3bc04ce53d6aea6bea1ed25a2acb9f06
codymoose <rcmoose2000@gmail.com>
2016-11-01 21:03:54 -0500

    Added toggleable Canvas size

--- web-applications/DigitalDoily/DigitalDoily.js
+++ web-applications/DigitalDoily/DigitalDoily.js
@@ -16,11 +16,14 @@ var gDiv;
 var bSlider;
 var bDiv;
 var downloadBtn;
+var toggleBtn;
+var w = window.innerWidth;
+var h = window.innerHeight;
 
 function setup(){
 	document.oncontextmenu = document.body.oncontextmenu = function() {return false;}
 	angleMode(DEGREES);
-	myCanvas = createCanvas(600, 600);
+	myCanvas = createCanvas(600,600);
 	translate(width / 2, height / 2);
 	reset();
 	resetBtn = createButton("Reset");
@@ -46,8 +49,12 @@ function setup(){
 	bDiv = createDiv("Blue: " + b);
 	bDiv.position(bSlider.x + bSlider.width + 5, bSlider.y);
 	
+	toggleBtn = createButton("Toggle Canvas Size");
+	toggleBtn.position(bSlider.x, bSlider.y + bSlider.height + 5);
+	toggleBtn.mousePressed(toggleSize);
+	
 	downloadBtn = createButton("Download");
-	downloadBtn.position(bSlider.x, bSlider.y + bSlider.height + 5);
+	downloadBtn.position(toggleBtn.x + toggleBtn.width + 5, bSlider.y + bSlider.height + 5);
 	downloadBtn.mousePressed(download);
 }
 
@@ -104,4 +111,27 @@ function reset(){
 
 function download() {
 	saveCanvas(myCanvas,'DigitalDoily', 'jpg');
+}
+
+function toggleSize() {
+	if(myCanvas.width == 600){
+		myCanvas.size(w, h);
+	} else {
+		myCanvas.size(600, 600);	
+	}
+	translate(width/2,height/2);
+	reset();
+	
+
+	resetBtn.position(5, height + 5);
+	sectorSlider.position(resetBtn.x, resetBtn.y + resetBtn.height + 5)
+	sectorDiv.position(sectorSlider.x + sectorSlider.width + 5, sectorSlider.y);
+	rSlider.position(sectorSlider.x, sectorSlider.y + sectorSlider.height + 5)
+	rDiv.position(rSlider.x + rSlider.width + 5, rSlider.y);
+	gSlider.position(rSlider.x, rSlider.y + rSlider.height + 5)
+	gDiv.position(gSlider.x + gSlider.width + 5, gSlider.y);
+	bSlider.position(gSlider.x, gSlider.y + gSlider.height + 5)
+	bDiv.position(bSlider.x + bSlider.width + 5, bSlider.y);
+	toggleBtn.position(bSlider.x, bSlider.y + bSlider.height + 5);
+	downloadBtn.position(toggleBtn.x + toggleBtn.width + 5, bSlider.y + bSlider.height + 5);
 }
